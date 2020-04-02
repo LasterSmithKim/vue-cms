2570349f6cee242c1a73a93048eb488fe76ca712
LasterSmithKim <kingone@yeah.net>
2020-04-02 22:41:14 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -6,27 +6,31 @@
 
         <!-- 中间的 路由 router-view 区域 -->
 
+        <transition>
+            <router-view></router-view>
+        </transition>
+
         <!-- 底部 Tabbar 区域 -->
         <nav class="mui-bar mui-bar-tab">
-			<a class="mui-tab-item" href="#tabbar">
-				<span class="mui-icon mui-icon-home"></span>
-				<span class="mui-tab-label">首页</span>
-			</a>
-			<a class="mui-tab-item mui-active" href="#tabbar-with-chat">
-				<span class="mui-icon mui-icon-email"><span class="mui-badge"></span></span>
-				<span class="mui-tab-label">消息</span>
-			</a>
-			<a class="mui-tab-item" href="#tabbar-with-contact">
-				<span class="mui-icon mui-icon-contact"></span>
-				<span class="mui-tab-label">通讯录</span>
-			</a>
-			<a class="mui-tab-item" href="#tabbar-with-map">
-				<span class="mui-icon mui-icon-gear"></span>
-				<span class="mui-tab-label">设置</span>
-			</a>
-		</nav>
-
-        <h1>这是 App 组件</h1>
+            <router-link class="mui-tab-item" to="/home">
+                <span class="mui-icon mui-icon-home"></span>
+                <span class="mui-tab-label">首页</span>
+            </router-link>
+            <router-link class="mui-tab-item" to="/member">
+                <span class="mui-icon mui-icon-contact"></span>
+                <span class="mui-tab-label">会员</span>
+            </router-link>
+            <router-link class="mui-tab-item" to="/shopcar">
+                <span class="mui-icon mui-icon-extra mui-icon-extra-cart">
+                    <span class="mui-badge">0</span>
+                </span>
+                <span class="mui-tab-label">购物车</span>
+            </router-link>
+            <router-link class="mui-tab-item" to="/search">
+                <span class="mui-icon mui-icon-search"></span>
+                <span class="mui-tab-label">搜索</span>
+            </router-link>
+        </nav>
     </div>
 </template>
 
@@ -37,5 +41,25 @@
 <style lang="scss" scoped>
 .app-container {
     padding-top: 40px;
+    overflow-x: hidden;
+}
+
+.v-enter {
+    opacity: 0;
+    transform: translateX(100%);
+}
+.v-leave-to {
+    opacity: 0;
+    transform: translateX(-100%);
+}
+
+.v-enter-active,
+.v-leave-active {
+    transition: all 0.6s ease;
+    position: absolute;
+}
+
+.v-move {
+    transition: all 0.6s ease;
 }
 </style>
--- src/router.js
+++ src/router.js
@@ -1,9 +1,21 @@
 import VueRouter from 'vue-router'
 
+//导入对应的路由组件
+import HomeContainer from './components/tabbar/HomeContainer.vue'
+import MemberContainer from './components/tabbar/MemberContainer.vue'
+import SearchContainer from './components/tabbar/SearchContainer.vue'
+import ShopcarContainer from './components/tabbar/ShopcarContainer.vue'
+
 
 var router = new VueRouter({
-    routes: [
-    ]
+    routes: [ //配置路由规则
+        { path: '/', redirect: '/home' },
+        { path: '/home', component: HomeContainer },
+        { path: '/member', component: MemberContainer },
+        { path: '/shopcar', component: ShopcarContainer },
+        { path: '/search', component: SearchContainer },
+    ],
+    linkActiveClass: 'mui-active'  // 覆盖 默认 路由高亮 的类，默认的叫 router-link-active 类
 })
 
 
